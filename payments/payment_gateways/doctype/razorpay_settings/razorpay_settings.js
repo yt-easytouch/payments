c05ee5f528e4d9f674f385f77521599a9786827f
ruthra kumar <ruthra@erpnext.com>
2024-10-25 14:51:02 +0530

    refactor: UI to clear razorpay api details

--- payments/payment_gateways/doctype/razorpay_settings/razorpay_settings.js
+++ payments/payment_gateways/doctype/razorpay_settings/razorpay_settings.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2016, Frappe Technologies and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Razorpay Settings', {
-	refresh: function(frm) {
 
-	}
+frappe.ui.form.on("Razorpay Settings", {
+  refresh: function (frm) {
+    frm.add_custom_button("Clear", function () {
+      frm.call({
+        doc: frm.doc,
+        method: "clear",
+        callback: function (r) {
+          frm.refresh();
+        },
+      });
+    });
+  },
 });
